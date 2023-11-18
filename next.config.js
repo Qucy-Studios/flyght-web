19078f14bfa9cd0880552b62f83ce9bf9c800530
Shindou Mihou <git@mihou.pw>
2023-11-18 12:44:57 +0800

    feat: update `next.config.js`

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactStrictMode: false
+    reactStrictMode: false,
+    output: 'export',
+    trailingSlash: true,
+    skipTrailingSlashRedirect: true,
+    compress: true
 }
 
 module.exports = nextConfig
