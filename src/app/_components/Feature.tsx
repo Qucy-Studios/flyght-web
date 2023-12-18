a05e6d7c03227eb4d1563665933ed615183a74cc
Shindou Mihou <git@mihou.pw>
2023-12-18 18:12:06 +0800

    feat: increase height for feature block

--- src/app/_components/Feature.tsx
+++ src/app/_components/Feature.tsx
@@ -2,7 +2,7 @@ import {LucideIcon} from "lucide-react";
 
 export default function Feature({ title, children, Icon }: { title: string, children: React.ReactNode, Icon: LucideIcon }) {
     return (
-        <div className={"flex flex-col gap-2 h-full md:max-h-[256px] relative w-full border-zinc-800 backdrop-blur bg-opacity-30 border rounded p-8"}>
+        <div className={"flex flex-col gap-2 h-full md:max-h-[300px] relative w-full border-zinc-800 backdrop-blur bg-opacity-30 border rounded p-8"}>
             <div className={"heropattern-graphpaper-zinc-900/50 absolute h-full w-full top-0 left-0 -z-20"}></div>
             <div className={"my-2"}>
                 <Icon size={24}></Icon>
