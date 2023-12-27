2eb2402fad1ebb3b9f5493d941d5b8c128f2f7be
Shindou Mihou <git@mihou.pw>
2023-12-27 17:44:28 +0800

    feat: highlight the premium button

--- src/app/_components/Header.tsx
+++ src/app/_components/Header.tsx
@@ -11,7 +11,7 @@ export const Header = () => {
           <div className={"flex flex-row justify-between gap-2 backdrop-blur bg-zinc-800 bg-opacity-30 select-none w-[80%] border rounded-full border-zinc-800 py-4 px-8 my-4"}>
               <a href={"/"} className={`${lora.className} font-bold lowercase text-2xl`}>Flyght.</a>
               <div className={"flex flex-row gap-2 items-center"}>
-                  <a href={"/premium"} rel={"external"} target={"_blank"} className={"text-sm text-zinc-400 clickable-hover-opacity"}>Premium</a>
+                  <a href={"/premium"} rel={"external"} target={"_blank"} className={"text-sm text-yellow-400 clickable-hover-opacity"}>Premium</a>
                   <p className={"text-sm text-zinc-400"}>•</p>
                   <a href={"/invite"}  rel={"external"} target={"_blank"} className={"text-sm text-zinc-400 clickable-hover-opacity"}>Invite</a>
                   <p className={"text-sm text-zinc-400"}>•</p>
