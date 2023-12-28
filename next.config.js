22953902c87a504ba873208ee912b8c3233a6127
Shindou Mihou <git@mihou.pw>
2023-12-28 11:37:51 +0800

    feat: use static redirects

--- next.config.js
+++ next.config.js
@@ -7,6 +7,30 @@ const nextConfig = {
     compress: true,
     eslint: {
         ignoreDuringBuilds: true,
+    },
+    async redirects() {
+      return [
+          {
+              source: '/premium',
+              destination: "https://buymeacoffee.com/flyght",
+              permanent: false
+          },
+          {
+              source: "/support",
+              destination: "https://discord.com/invite/kxcUJ77SSF",
+              permanent: false
+          },
+          {
+              source: "/discord",
+              destination: "https://discord.com/invite/kxcUJ77SSF",
+              permanent: false
+          },
+          {
+              source: "/invite",
+              destination: "https://discord.com/api/oauth2/authorize?client_id=1173284134945308742&permissions=1099780418630&scope=applications.commands%20bot",
+              permanent: false
+          }
+      ]
     }
 }
 
